c6babe65c5e951117fe02216e8b695d707ec3f66
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-12-08 17:48:21 -0600

    Added env vars and registrations ns app

--- permissions_melectric/settings.py
+++ permissions_melectric/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ky8k9z3h@sh_y@a75*hnhnb9k50^b51#o67yrzq5%8c(utm2=a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +77,33 @@ WSGI_APPLICATION = 'permissions_melectric.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {},
+        # 'NAME': 'permissions_melectric2_dev',
+        'NAME': os.environ.get("DB_NAME"),
+        # 'NAME': os.getenv("DB_NAME", ''),
+        # 'USER': 'postgres',
+        # 'USER': os.getenv('DB_USER', ''),
+        'USER': os.environ.get("DB_USER"),
+        # 'USER': env("DB_USER"),
+        # 'PASSWORD': 'postgres',
+        # 'PASSWORD': os.getenv('DB_PASS', ''),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        #  'HOST': 'localhost',
+        # 'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+        }
     }
-}
 
 
 # Password validation
